e91cf0edeadc025a97f2216cd0564181a9a8d311
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-05-28 15:37:21 +0500

    add

--- login.js
+++ login.js
@@ -1,23 +1,23 @@
 
-import { auth, signInWithEmailAndPassword} from './firebase.js';
+import { auth, signInWithEmailAndPassword } from './firebase.js';
 
 
 let log = () => {
-let logEmail = document.getElementById('logEmail');
-let logPassword = document.getElementById('logPassword');
+    let logEmail = document.getElementById('logEmail');
+    let logPassword = document.getElementById('logPassword');
 
-signInWithEmailAndPassword(auth, logEmail.value, logPassword.value)
-  .then((userCredential) => {
-    
-    const user = userCredential.user;
-    window.location.assign('profile.html')
+    signInWithEmailAndPassword(auth, logEmail.value, logPassword.value)
+        .then((userCredential) => {
 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    swal('Invalid Candidiate')
-  });
+            const user = userCredential.user;
+            window.location.assign('profile.html')
+
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            swal('Invalid Candidiate')
+        });
 }
 
 let login = document.getElementById('login');
